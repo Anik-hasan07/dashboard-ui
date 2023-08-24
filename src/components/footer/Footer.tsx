d0bc3b388bdd3b439e02e208215a7a0739de9047
Anik-hasan07 <iamakashcse@gmail.com>
2023-08-24 21:58:47 +0600

    footer name changed

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,7 +4,7 @@ import "./footer.scss"
 const Footer = () => {
   return (
     <div className="footer">
-      <span>lamadmin</span>
+      <span>lamanikhasan</span>
       <span>© Lama Dev Admin Dashboard</span>
     </div>
   )
